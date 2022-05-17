4266328f31634960ecbf08949e19ddd0d3190fa5
Robert <osipovitchr@gmail.com>
2022-05-17 15:48:23 -0400

    POST request to server from client complete

--- src/pages/Compose/Components/ComposeForm/ComposeForm.js
+++ src/pages/Compose/Components/ComposeForm/ComposeForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 import DisplayModal from '../../../../SharedComponents/DisplayModal/DisplayModal'
 import Dictionary from './localComponents/Dictionary/Dictionary'
@@ -10,13 +11,35 @@ const ComposeForm = () => {
     const [imageInput, setImageInput] = useState('')
     const [creatorInput, setCreatorInput] = useState('')
     const [showModal, setShowModal] = useState(false);
+    const navigate = useNavigate()
 
     const modalClickOn = () => {
       setShowModal(true)
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const handleClick = (e) => {
+        // e.preventDefault()
+
+       const body = {
+            title: titleInput,
+            content: contentInput,
+            imageUrl: imageInput,
+            creator: creatorInput
+        }
+
+        fetch('http://localhost:5000/feed/posts', {
+          method: 'POST',
+          body: JSON.stringify(body),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(data => {
+            console.log('data from post--', data)
+            return navigate("/read", {replace: true})
+          })
+          .catch(err => console.log('.catch block of Create Post', err))
     }
   
   return (
@@ -24,7 +47,7 @@ const ComposeForm = () => {
     <DisplayModal setShowModal={setShowModal} showModal={showModal} component={Dictionary} />
     <div className={classes['form-container']}>
         <button onClick={modalClickOn} className={classes.openDictionary}>Need help with a word? Click Me!</button>
-        <form className={classes.form}>
+        {/* <form className={classes.form}> */}
         
         <label className={classes.label} htmlFor="title">Title</label>
         <input name="title" id="title" className={classes.input} onChange={(e) => setTitleInput(e.target.value)} />
@@ -38,8 +61,8 @@ const ComposeForm = () => {
         <label className={classes.label} htmlFor="content">Write your Post!</label>
         <textarea rows="20" cols="100" name="content" id="content" className={classes.textarea} onChange={(e) => setContentInput(e.target.value)} />
 
-        <button className={classes.button} type="submit" onSubmit={(e) => handleSubmit(e)}>Post It!</button>
-        </form>
+        <button className={classes.button} onClick={() => handleClick()}>Post It!</button>
+        {/* </form> */}
     </div>
       </>
   )
